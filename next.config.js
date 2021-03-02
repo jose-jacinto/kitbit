a3af47d4d55fefded42517c8406e13358e62b599
Diogo-k <diogo.paulo@cottonhat.net>
2021-03-02 15:20:37 +0000

    New checkout

--- next.config.js
+++ next.config.js
@@ -8,34 +8,34 @@ module.exports = {
     locales: ['en-US', 'pt'],
     defaultLocale: 'en-US',
   },
-  async rewrites() {
-    return [
-      // Rewrites for /search
-      {
-        source: '/:locale/search',
-        destination: '/search',
-      },
-      {
-        source: '/:locale/search/:path*',
-        destination: '/search',
-      },
-      {
-        source: '/search/designers/:name',
-        destination: '/search',
-      },
-      {
-        source: '/search/designers/:name/:category',
-        destination: '/search',
-      },
-      {
-        // This rewrite will also handle `/search/designers`
-        source: '/search/:category',
-        destination: '/search',
-      },
-      {
-        source: '/:path/checkout',
-        destination: 'https://d3v0kwj6fsyaja.cloudfront.net/1.0/index.html'
-      },
-    ]
-  },
+  // async rewrites() {
+  //   return [
+  //     // Rewrites for /search
+  //     {
+  //       source: '/:locale/search',
+  //       destination: '/search',
+  //     },
+  //     {
+  //       source: '/:locale/search/:path*',
+  //       destination: '/search',
+  //     },
+  //     {
+  //       source: '/search/designers/:name',
+  //       destination: '/search',
+  //     },
+  //     {
+  //       source: '/search/designers/:name/:category',
+  //       destination: '/search',
+  //     },
+  //     {
+  //       // This rewrite will also handle `/search/designers`
+  //       source: '/search/:category',
+  //       destination: '/search',
+  //     },
+  //     {
+  //       source: '/:path/checkout',
+  //       destination: 'https://d3v0kwj6fsyaja.cloudfront.net/1.0/index.html'
+  //     },
+  //   ]
+  // },
 }
