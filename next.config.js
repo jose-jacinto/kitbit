d4338014794a5884b10768f3cfcb1e3941dcba4a
Jose Jacinto <jacinto.jd@gmail.com>
2021-03-03 10:53:54 +0000

    Update vercel

--- next.config.js
+++ next.config.js
@@ -8,4 +8,16 @@ module.exports = {
     locales: ['en-US', 'pt'],
     defaultLocale: 'en-US',
   },
+  async rewrites() {
+    return [
+      {
+        "source": "/checkout",
+        "destination": "https://d3v0kwj6fsyaja.cloudfront.net/1.0/index.html"
+      },
+      {
+          "source": "/checkout/:match*",
+          "destination": "https://d3v0kwj6fsyaja.cloudfront.net/1.0/:match*"
+    }
+    ];
+  }
 }
