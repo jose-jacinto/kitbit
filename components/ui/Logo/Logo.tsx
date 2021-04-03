268da6b8d12c85678db5493f59dea94a26f8c445
Jose <jacinto.jd@gmail.com>
2021-04-03 22:22:25 +0100

    Changed logo

--- components/ui/Logo/Logo.tsx
+++ components/ui/Logo/Logo.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Logo = ({ className = '', ...props }) => {
   return (
-    <img src="/kitbit_logo_normal.png" style={{ width: props.width, height: 'auto' }} />
+    <img src="/kitbit_logo_normal.png" style={{ width: `${props.width}px`, height: 'auto' }} />
   );
 }
 export default Logo;
